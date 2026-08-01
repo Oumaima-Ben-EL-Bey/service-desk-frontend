8a27014bf678ffdc0b41314217a1438adc4c55f0
Oumaima Ben El Bey <101521347+Oumaima-Ben-EL-Bey@users.noreply.github.com>
2026-08-01 14:05:50 +0200

    feat: add create-ticket form with client-side validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Tickets } from './tickets/tickets';
 import { TicketDetail } from './ticket-detail/ticket-detail';
+import { CreateTicket } from './create-ticket/create-ticket';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'tickets', component: Tickets },
+  { path: 'tickets/new', component: CreateTicket },
   { path: 'tickets/:id', component: TicketDetail },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
