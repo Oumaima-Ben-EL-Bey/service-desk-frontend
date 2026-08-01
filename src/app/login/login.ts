2809d8774b6b8469525f3c7cbf775543804d4c59
Oumaima Ben El Bey <101521347+Oumaima-Ben-EL-Bey@users.noreply.github.com>
2026-08-01 22:09:37 +0200

    feat: add registration screen linked with login (#22)

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,27 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Auth } from '../auth';
+
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-
   private http = inject(HttpClient);
   private router = inject(Router);
   private auth = inject(Auth);
-
 
   email = '';
   password = '';
 
   onLogin() {
-
     this.http
       .post<{ token: string }>('https://service-desk-api.fly.dev/login', {
         email: this.email,
